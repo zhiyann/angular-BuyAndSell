f184dffb20204f7923787aee6d5967a81365e0b4
zhiyann <goh.zhiyan@gmail.com>
2019-11-12 00:18:50 +0800

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
